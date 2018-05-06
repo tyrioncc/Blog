fa978cf974b869829b26ec8a1dbbf3778dae3d1c
tyrioncc <chengjy42@gmail.com>
2018-05-06 21:35:06 +0800

    2018/5/6

--- src/test/java/dev/blog/JooqExample.java
+++ src/test/java/dev/blog/JooqExample.java
@@ -7,6 +7,7 @@ import org.jooq.Result;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jooq.AutoConfigureJooq;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,10 +15,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.jooq.impl.DSL.field;
 import static org.jooq.impl.DSL.table;
 
+//import static org.jooq.example.db.mysql.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Component
+@AutoConfigureJooq
 public class JooqExample
 {
     @Autowired
@@ -29,9 +33,17 @@ public class JooqExample
                 create.select(field("title")).from(table("articles")).where(field("id").eq(id)).fetch();
         return result.getValues(0).toString();
     }
+/*
+    private String getArticleTitle2(int id) {
+        Result<Record1<Object>> result =
+                create.select(TITLE).from(ARTICLES).where(ID.eq(id)).fetch();
+        return result.getValues(0).toString();
+    }*/
+
 
     @Test
     public void testGetArticleTitle(){
         System.out.println(getArticleTitle(1));
+        //System.out.println(getArticleTitle2(1));
     }
 }
