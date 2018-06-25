eac875d118f86ecd0c1f67c3769730312db5db9e
tyrioncc <chengjy42@gmail.com>
2018-06-25 20:38:27 +0800

    2018/6/25

---
+++ src/main/resources/static/js/MDToHtml.js
@@ -0,0 +1,13 @@
+$(function() {
+    console.log("md to html");
+    var editormdView;
+
+    editormdView = editormd.markdownToHTML("articleContent", {
+        htmlDecode      : "style,script,iframe",  // you can filter tags decode
+        emoji           : true,
+        taskList        : true,
+        tex             : true,  // 默认不解析
+        flowChart       : true,  // 默认不解析
+        sequenceDiagram : true,  // 默认不解析
+    });
+});
