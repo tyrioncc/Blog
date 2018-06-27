12456e073cfa2437f04b34fa49c7c15f89ae2bcc
tyrioncc <chengjy42@gmail.com>
2018-06-27 15:16:15 +0800

    2018/6/27

--- src/main/java/dev/blog/Application.java
+++ src/main/java/dev/blog/Application.java
@@ -1,13 +1,19 @@
 package dev.blog;
 
+import dev.blog.config.StorageProperties;
+import dev.blog.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
 
@@ -15,4 +21,11 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
---
+++ src/main/java/dev/blog/exception/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package dev.blog.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
